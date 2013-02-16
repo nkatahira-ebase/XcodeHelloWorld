6a7b539f5167a17914b9cf3790cd0978bb3fd8c7
nkatahira <nkatahira@ebase-solutions.com>
2013-02-16 18:17:19 +0900

    てっすとー

--- HelloWorld/AppDelegate.h
+++ HelloWorld/AppDelegate.h
@@ -3,7 +3,7 @@
 //  HelloWorld
 //
 //  Created by katahira on 2013/01/29.
-//  Copyright (c) 2013年 QualitySeason. All rights reserved.
+//  Copyright (c) 2013年 QualitySeason. All rights reserved.//
 //
 
 #import <UIKit/UIKit.h>
